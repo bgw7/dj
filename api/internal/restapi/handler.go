de9bac46ac6d9d01b7adf9bbc6703ad4c1553cec
bgw7 <26874296+bgw7@users.noreply.github.com>
2024-10-09 21:34:15 -0400

    use header

--- api/internal/restapi/handler.go
+++ api/internal/restapi/handler.go
@@ -79,9 +79,9 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		r.Use(metdataMiddleware)
 		r.Get("/", handleOut(h.service.ListTracks, http.StatusOK))
 		r.Post("/", handleInOut(h.service.CreatTrack, http.StatusCreated))
-		r.Route("/dl/{url}", func(r chi.Router) {
+		r.Route("/dl", func(r chi.Router) {
 			r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-				url := chi.URLParam(r, "url")
+				url := r.Header.Get("url")
 				v, err := termux.YoutubeDownload(url)
 				if err != nil {
 					handleError(w, err)
