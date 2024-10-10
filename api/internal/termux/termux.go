e1964c0951de00028d30a2d36ea476a6ea99c068
bgw7 <26874296+bgw7@users.noreply.github.com>
2024-10-09 21:35:30 -0400

    log

--- api/internal/termux/termux.go
+++ api/internal/termux/termux.go
@@ -6,6 +6,7 @@ import (
 )
 
 func YoutubeDownload(youtubeShareLink string) (string, error) {
+	fmt.Println(youtubeShareLink)
 	out, err := exec.Command("termux-url-opener", youtubeShareLink).Output()
 	fmt.Println("out from youtube download", string(out))
 	if err != nil {
